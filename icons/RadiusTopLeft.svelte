1e25a19ae3d3990346f656454f0af84b270724e8
Tyler Kieft <tyler@nokto.ai>
2022-04-19 14:26:52 -0400

    regenerate

--- icons/RadiusTopLeft.svelte
+++ icons/RadiusTopLeft.svelte
@@ -11,6 +11,7 @@
 
 <svg
     bind:this="{element}"
+    on:click
     xmlns="http://www.w3.org/2000/svg"
     class="{icon ? 'icon' : ''} icon-tabler icon-tabler-radius-top-left {classes}"
     width="{size}"
--- icons/TrafficLights.svelte
+++ icons/TrafficLights.svelte
@@ -11,6 +11,7 @@
 
 <svg
     bind:this="{element}"
+    on:click
     xmlns="http://www.w3.org/2000/svg"
     class="{icon ? 'icon' : ''} icon-tabler icon-tabler-traffic-lights {classes}"
     width="{size}"
